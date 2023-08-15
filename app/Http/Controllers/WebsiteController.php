fb176c0aa774790367d68fa800421ccd6227d648
Abdullah Can <AbdullahCan10110@gmail.com>
2023-08-15 16:42:06 +0300

    Initial commit

--- app/Http/Controllers/WebsiteController.php
+++ app/Http/Controllers/WebsiteController.php
@@ -9,49 +9,49 @@ class WebsiteController extends Controller
     public function index() {
 
         $page['page'] = 'home' ;
-        return view('index',$page);
+        return view('pages.index',$page);
 
     }
 
     public function about() {
 
         $page['page'] = 'about' ;
-        return view('about',$page);
+        return view('pages.about',$page);
 
     }
 
     public function eventDetails() {
 
         $page['page'] = 'eventDetails' ;
-        return view('eventDetails',$page);
+        return view('pages.eventDetails',$page);
 
     }
 
     public function rentVenue() {
 
         $page['page'] = 'rentVenue';
-        return view('rentVenue',$page);
+        return view('pages.rentVenue',$page);
 
     }
 
     public function showsEvents() {
 
         $page['page'] = 'showsEvents' ;
-        return view('showsEvents',$page);
+        return view('pages.showsEvents',$page);
 
     }
 
     public function ticketDetails() {
 
         $page['page'] = 'ticketDetails' ;
-        return view('ticketDetails',$page);
+        return view('pages.ticketDetails',$page);
 
     }
 
     public function tickets() {
 
         $page['page'] = 'tickets' ;
-        return view('tickets',$page);
+        return view('pages.tickets',$page);
 
     }
 
